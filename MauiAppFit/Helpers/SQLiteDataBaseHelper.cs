95cd11378cf725b71ce4a58df7c9a126b167609f
231101 <231101@CPD02-M06>
2025-07-28 10:53:52 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppFit.Helpers
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection _db;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+
+        }
+
+    }
+}
